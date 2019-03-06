5a7aa6beb1af73411c9befa0d3cfd6d2547a9929
Suree1988 <44668926+Suree1988@users.noreply.github.com>
2019-03-06 08:03:10 +0100

    Update config.php

--- grupparbete_backend/app/config/config.php
+++ grupparbete_backend/app/config/config.php
@@ -1,14 +1,14 @@
 <?php
   // DB Params
-  define("DB_HOST", "localhost");
-  define("DB_USER", "root");
-  define("DB_PASS", "sureerat2531");
+  define("DB_HOST", "");
+  define("DB_USER", "");
+  define("DB_PASS", "");
   define("DB_NAME", "bookshop");
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root (eg. http://myapp.com or http://localhost/myapp)
-  define('URLROOT', 'http://localhost/grupparbete_backend');
+  define('URLROOT', 'http://');
   // Site Name
   define('SITENAME', 'Threeboxes bookshop Online');
   // App Version
